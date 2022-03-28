47e3e05ea1045d628441459e6cf0dc8beedca0c4
t.he <study.hetao@foxmail.com>
2022-03-28 16:55:41 +0800

    增加消息可靠性方案 + 重试配置

--- mq/src/main/java/com/github/crybooy007/mq/rabbitmq/config/DeclareRabbitModule.java
+++ mq/src/main/java/com/github/crybooy007/mq/rabbitmq/config/DeclareRabbitModule.java
@@ -48,6 +48,12 @@ public class DeclareRabbitModule implements SmartInitializingSingleton {
 					arguments.put("x-dead-letter-routing-key", rabbitEnum.getRouteKey());
 					Queue queue = new Queue(rabbitEnum.getQueue(), true,false,false, arguments);
 					amqpAdmin.declareBinding(BindingBuilder.bind(queue).to(directExchange).with(rabbitEnum.getQueue()));*/
+					/**
+					 * 注意：在同时启用消息回退机制和备份交换机的情况下，备份交换机的优先级高于消息回退机制。
+					 * 也就是说，无法路由到队列的消息将直接交给备份交换机，而不会回退给生产者的回调方法
+					 *
+					 * ExchangeBuilder.directExchange("confirm.exchange").alternate() alternate指定备份交换机
+					 */
 				}else {
 					DirectExchange directExchange = new DirectExchange(rabbitEnum.getExchange(), true, false);
 					Queue queue = new Queue(rabbitEnum.getQueue(), true);
