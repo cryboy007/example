20f62c60bfc85b0969e68a1b4911d14133f8f93d
t.he <study.hetao@foxmail.com>
2023-03-05 12:09:22 +0800

    Writing an Echo Server

--- netty/src/main/java/com/github/cryboy007/netty/discard/DiscardServerHandler.java
+++ netty/src/main/java/com/github/cryboy007/netty/discard/DiscardServerHandler.java
@@ -1,10 +1,7 @@
 package com.github.cryboy007.netty.discard;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.CharsetUtil;
-import io.netty.util.ReferenceCountUtil;
 
 /**
  *@ClassName DiscardServerHandler
@@ -14,18 +11,9 @@ import io.netty.util.ReferenceCountUtil;
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		ByteBuf in = (ByteBuf) msg;
-		try {
-//			while (in.isReadable()) {
-//				System.out.println((char) in.readByte());
-//				System.out.flush();
-//			}
-			System.out.println(in.toString(CharsetUtil.UTF_8));
-		}
-		finally {
-			//in.release();
-			ReferenceCountUtil.release(msg);
-		}
+//		ctx.write(msg);
+//		ctx.flush();
+		ctx.writeAndFlush(msg);
 	}
 
 	@Override
